fa26b1fc878d6f77b56bb2a35bb641767fe14b7c
JordanM03 <jordanmontigiani28@gmail.com>
2022-10-18 22:05:39 +0200

    j'ai envoyé une inscription

--- modele/inscription.php
+++ modele/inscription.php
@@ -1,23 +1,14 @@
 <?php
+require("controller\database.php");
+
+$nomU = $_POST["nom"];
+$prenomU = $_POST["prenom"];
+$emailU = $_POST["email"];
+$passwordU = $_POST["motdepasse"];
+var_dump($_POST);
+
+$pers = new Database();
+$pers->AddUser($nomU, $prenomU, $emailU, md5($passwordU));
+echo "Welcome on WERJ dear " .$prenomU ."!";
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscription</title>
-    <style>
-
-    </style>
-</head>
-<body>
-    <h1>Inscription</h1>
-    <form action="" method="post">
-        Nom: <input type="text" name="nom" placeholder="Tapez votre nom...">
-        <input type="submit" value="Envoyer">
-    </form>
-</body>
-</html>
