d408b4e39379a8209c5acd3ef84b331df1a23335
Wildrey237 <wilfriedbemelingue@gmail.com>
2022-10-19 12:02:41 +0200

    base de donnée

--- sql/leboncoin.sql
+++ sql/leboncoin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 18 oct. 2022 à 20:54
+-- Généré le : mer. 19 oct. 2022 à 10:02
 -- Version du serveur : 8.0.27
 -- Version de PHP : 7.4.26
 
@@ -24,58 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `annonce`
---
-
-DROP TABLE IF EXISTS `annonce`;
-CREATE TABLE IF NOT EXISTS `annonce` (
-  `id_Annonce` int NOT NULL AUTO_INCREMENT,
-  `nom_annonce` varchar(45) DEFAULT NULL,
-  `detail` varchar(2000) DEFAULT NULL,
-  `date_ajout` date DEFAULT NULL,
-  `USER_id_user` int NOT NULL,
-  `categorie_id_categorie` int NOT NULL,
-  `Media` varchar(2000) DEFAULT NULL,
-  PRIMARY KEY (`id_Annonce`,`USER_id_user`,`categorie_id_categorie`),
-  UNIQUE KEY `id_Annonce_UNIQUE` (`id_Annonce`),
-  KEY `fk_Annonce_USER1_idx` (`USER_id_user`),
-  KEY `fk_Annonce_categorie1_idx` (`categorie_id_categorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `categorie`
---
-
-DROP TABLE IF EXISTS `categorie`;
-CREATE TABLE IF NOT EXISTS `categorie` (
-  `id_categorie` int NOT NULL AUTO_INCREMENT,
-  `nom_categorie` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id_categorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `message`
---
-
-DROP TABLE IF EXISTS `message`;
-CREATE TABLE IF NOT EXISTS `message` (
-  `id_message` int NOT NULL AUTO_INCREMENT,
-  `detail` varchar(45) DEFAULT NULL,
-  `Annonce_id_Annonce` int NOT NULL,
-  `USER_id_user` int NOT NULL,
-  PRIMARY KEY (`id_message`,`Annonce_id_Annonce`,`USER_id_user`),
-  UNIQUE KEY `Annonce_id_Annonce_UNIQUE` (`Annonce_id_Annonce`),
-  KEY `fk_message_Annonce_idx` (`Annonce_id_Annonce`),
-  KEY `fk_message_USER1_idx` (`USER_id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `user`
 --
 
@@ -88,25 +36,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `mot_de_passe` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_user`),
   UNIQUE KEY `id_user_UNIQUE` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
 
 --
--- Contraintes pour les tables déchargées
+-- Déchargement des données de la table `user`
 --
 
---
--- Contraintes pour la table `annonce`
---
-ALTER TABLE `annonce`
-  ADD CONSTRAINT `fk_Annonce_categorie1` FOREIGN KEY (`categorie_id_categorie`) REFERENCES `categorie` (`id_categorie`),
-  ADD CONSTRAINT `fk_Annonce_USER1` FOREIGN KEY (`USER_id_user`) REFERENCES `user` (`id_user`);
-
---
--- Contraintes pour la table `message`
---
-ALTER TABLE `message`
-  ADD CONSTRAINT `fk_message_Annonce` FOREIGN KEY (`Annonce_id_Annonce`) REFERENCES `annonce` (`id_Annonce`),
-  ADD CONSTRAINT `fk_message_USER1` FOREIGN KEY (`USER_id_user`) REFERENCES `user` (`id_user`);
+INSERT INTO `user` (`id_user`, `nom_user`, `prenom_user`, `mail_user`, `mot_de_passe`) VALUES
+(5, 'Bemelingue Djossie', 'Wilfried Ryan', 'wilfriedbemelingue@gmail.com', 'ebe596017db2f8c69136e5d6e594d365'),
+(6, 'montigiani', 'jordan', 'jordan@gmail.com', 'e9dd8698f46ee082688b195ea4676152'),
+(7, 'Bemelingue Djossie', 'Wilfried Ryan', 'wilfriedbemelingue@wil.com', 'ebe596017db2f8c69136e5d6e594d365');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
