80ad5c4f9cd240c59b489e3671ca9a040fb16044
Wildrey237 <wilfriedbemelingue@gmail.com>
2022-10-22 18:13:00 +0200

    ajout de like

--- controller/database.php
+++ controller/database.php
@@ -18,119 +18,132 @@ class Database
     }
 
 
-
     // user function
 
-    public function GetUsers(){
+    public function GetUsers()
+    {
         $sql = 'SELECT * FROM user';
         $statement = self::$database->prepare($sql);
         $statement->execute();
         return $statement->fetchAll();
     }
 
-    public function ChangePassword($id, $newPassword){
+    public function ChangePassword($id, $newPassword)
+    {
         $sql = "UPDATE `user` SET `mot_de_passe` = :password WHERE `user`.`id_user` = :id";
         $statement = self::$database->prepare($sql);
-        $statement->execute(array(":password" => md5($newPassword),":id"  => $id));
+        $statement->execute(array(":password" => md5($newPassword), ":id" => $id));
     }
 
-    public function AddUser($nomuser, $prenomuser, $mailuser, $motdepasse){
+    public function AddUser($nomuser, $prenomuser, $mailuser, $motdepasse)
+    {
         $sql = "INSERT INTO `user` (`nom_user`, `prenom_user`, `mail_user`, `mot_de_passe`)
                 VALUES (:nomuser, :prenomuser, :mail, :motdepasse)";
         $statement = self::$database->prepare($sql);
-        $statement->execute(array(":nomuser"=>$nomuser,":prenomuser"=>$prenomuser,":mail"=>$mailuser,":motdepasse"=>$motdepasse));
+        $statement->execute(array(":nomuser" => $nomuser, ":prenomuser" => $prenomuser, ":mail" => $mailuser, ":motdepasse" => $motdepasse));
     }
 
-    public function DeletteUser($id){
+    public function DeletteUser($id)
+    {
         $sql = "DELETE FROM `user` WHERE `user`.`id_user` = :iduser";
         $statement = self::$database->prepare($sql);
-        $statement->bindParam(":iduser",$id,PDO::PARAM_INT);
+        $statement->bindParam(":iduser", $id, PDO::PARAM_INT);
         $statement->execute();
     }
 
-    public function Connect($mail, $password){
+    public function Connect($mail, $password)
+    {
         $sql = "SELECT * FROM `user`
                 WHERE mail_user = :mail
                 AND mot_de_passe = :password";
         $statement = self::$database->prepare($sql);
-        $statement->execute(array(":mail" => $mail, ":password" => $password ));
+        $statement->execute(array(":mail" => $mail, ":password" => $password));
         return $statement->fetchAll();
     }
 
 
     // Annonce Function
 
-    public function AddAnnnonce ($nom_annonce, $detail, $prix, $date, $id_user, $id_categorie, $media){ // ajouter des annonces
+    public function AddAnnnonce($nom_annonce, $detail, $prix, $date, $id_user, $id_categorie, $media)
+    { // ajouter des annonces
         $sql = "INSERT INTO `annonce` (`id_Annonce`, `nom_annonce`, `prix`, `detail`, `date_ajout`, `USER_id_user`, `categorie_id_categorie`, `Media`) 
                 VALUES (NULL, :nom_annonce, :prix, :detail, :date, :id_user, :id_categorie, :media)";
         $statement = self::$database->prepare($sql);
-        $statement->execute(array(":nom_annonce" => $nom_annonce, ":detail" => $detail, ":prix" =>$prix, ":date" => $date, ":id_user" => $id_user, ":id_categorie" => $id_categorie, ":media" => $media));
+        $statement->execute(array(":nom_annonce" => $nom_annonce, ":detail" => $detail, ":prix" => $prix, ":date" => $date, ":id_user" => $id_user, ":id_categorie" => $id_categorie, ":media" => $media));
     }
 
-    public function AlterAnnnonce ($id_annonce,$nom_annonce, $prix, $detail, $date, $id_user, $id_categorie, $media){ //modifier les annonces
+    public function AlterAnnnonce($id_annonce, $nom_annonce, $prix, $detail, $date, $id_user, $id_categorie, $media)
+    { //modifier les annonces
         $sql = "UPDATE `annonce` SET `nom_annonce` = :nom_annonce, `prix` = :prix,`detail` = :detail, `date_ajout` = :date, `Media` = :Media, `categorie_id_categorie` =:id_categorie
                  WHERE `annonce`.`id_Annonce` = {$id_annonce}
                    AND `annonce`.`USER_id_user` = {$id_user}";
         $statement = self::$database->prepare($sql);
-        $statement->execute(array(":nom_annonce" => $nom_annonce, ":prix" => $prix, ":detail" => $detail, ":date" => $date, ":id_categorie" => $id_categorie, ":Media" => $media ));
+        $statement->execute(array(":nom_annonce" => $nom_annonce, ":prix" => $prix, ":detail" => $detail, ":date" => $date, ":id_categorie" => $id_categorie, ":Media" => $media));
         //TODO : Invalid parameter number: number of bound variables does not match number of tokens
     }
 
 
-    public function GetAnnonceFromUser($id_user){ //permet de recuperer les annonces publiers par un utilisateur
+    public function GetAnnonceFromUser($id_user)
+    { //permet de recuperer les annonces publiers par un utilisateur
         $sql = 'SELECT * FROM `annonce` WHERE `USER_id_user`=:id_user;';
         $statement = self::$database->prepare($sql);
-        $statement->bindParam(":id_user",$id_user,PDO::PARAM_INT);
+        $statement->bindParam(":id_user", $id_user, PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetchAll();
     }
 
-    public function GetAnnonceFromUserAndId($id_user, $id_annonce){ //permet de recuperer les annonces publiers par un utilisateur
+    public function GetAnnonceFromUserAndId($id_user, $id_annonce)
+    { //permet de recuperer les annonces publiers par un utilisateur
         $sql = 'SELECT * FROM `annonce` WHERE `USER_id_user`=:id_user AND id_Annonce = :id_annonce;';
         $statement = self::$database->prepare($sql);
-        $statement->bindParam(":id_user",$id_user,PDO::PARAM_INT);
-        $statement->bindParam(":id_annonce",$id_annonce,PDO::PARAM_INT);
+        $statement->bindParam(":id_user", $id_user, PDO::PARAM_INT);
+        $statement->bindParam(":id_annonce", $id_annonce, PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetchAll();
     }
 
-    public function GetAnnonce(){ //recuperer toutes les annonces
+    public function GetAnnonce()
+    { //recuperer toutes les annonces
         $sql = 'SELECT * FROM `annonce`';
         $statement = self::$database->prepare($sql);
         $statement->execute();
         return $statement->fetchAll();
     }
 
-    public function getAnnonceByFilters($keyword,$categoryID,$price){ //recuperer  les annonces par filters
+    public function getAnnonceByFilters($keyword, $categoryID, $price)
+    { //recuperer  les annonces par filters
         $sql = "SELECT * FROM `annonce` WHERE  `categorie_id_categorie` = :id_categorie  AND `prix` <= :prix  AND `nom_annonce` LIKE '%{$keyword}%'  ";
- 
+
         $statement = self::$database->prepare($sql);
-        $statement->bindParam(":id_categorie",$categoryID,PDO::PARAM_INT);
-        $statement->bindParam(":prix",$price,PDO::PARAM_INT);
+        $statement->bindParam(":id_categorie", $categoryID, PDO::PARAM_INT);
+        $statement->bindParam(":prix", $price, PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetchAll();
     }
 
-    public function getAnnonceByID( $id ){ //recuperer les annonces par filters
+    public function getAnnonceByID($id)
+    { //recuperer les annonces par filters
         $sql = "SELECT * FROM `annonce` WHERE  `id_Annonce` = :id_annonce  ";
- 
+
         $statement = self::$database->prepare($sql);
-        $statement->bindParam(":id_annonce",$id_annonce,PDO::PARAM_INT);
+        $statement->bindParam(":id_annonce", $id_annonce, PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetchAll()[0];
     }
 
 
-    public function DeletteAnnonce($id_annonce, $id_user){ //supprimer une annonce
+    public function DeletteAnnonce($id_annonce, $id_user)
+    { //supprimer une annonce
         $sql = "DELETE FROM annonce WHERE annonce.id_Annonce= :id_Annonce AND `USER_id_user` = {$id_user}";
         $statement = self::$database->prepare($sql);
-        $statement->bindParam(":id_Annonce",$id_annonce,PDO::PARAM_INT);
+        $statement->bindParam(":id_Annonce", $id_annonce, PDO::PARAM_INT);
         $statement->execute();
     }
 
     // Categorie Function
 
-    public function GetCategorie(){ //choisir une categorie
+    public function GetCategorie()
+    { //choisir une categorie
         $sql = 'SELECT * FROM `categorie`';
         $statement = self::$database->prepare($sql);
         $statement->execute();
@@ -138,16 +151,18 @@ class Database
     }
 
     // FUNCTION POUR LA PAGE ADMIN SI ON A FINI
-    public function AddCategorie ($nom_categorie){ //ajouter une categorie
+    public function AddCategorie($nom_categorie)
+    { //ajouter une categorie
         $sql = "INSERT INTO `categorie` (`id_categorie`, `nom_categorie`) 
                 VALUES (NULL, :nom_categorie)";
         $statement = self::$database->prepare($sql);
-        $statement->bindParam(":iduser",$nom_categorie,PDO::PARAM_INT);
+        $statement->bindParam(":iduser", $nom_categorie, PDO::PARAM_INT);
         $statement->execute();
     }
 
     // Gestion des messages
-    public function AddMessage($detail, $id_annonce, $id_user){ //pour ajouter un message
+    public function AddMessage($detail, $id_annonce, $id_user)
+    { //pour ajouter un message
         $sql = "INSERT INTO `message` (`id_message`, `detail`, `Annonce_id_Annonce`, `USER_id_user`) 
                 VALUES (NULL, :detail, :id_annonce, :id_user)";
         $statement = self::$database->prepare($sql);
@@ -155,23 +170,26 @@ class Database
     }
 
     // affichages des messages selon un utilisateur et le createur de l'annonce
-    public function ShowUserMessage($id_annonce, $id_user){
+    public function ShowUserMessage($id_annonce, $id_user)
+    {
         $sql = "SELECT message.detail as 'message', user.nom_user as 'recepteur', annonce.nom_annonce as 'Annonce', message.USER_id_user AS 'emmeteur'
                 FROM message, annonce, user
                 WHERE Annonce_id_Annonce = annonce.id_Annonce
                 AND annonce.USER_id_user = user.id_user
                 AND message.USER_id_user = :iduser
                 AND annonce.id_Annonce = :idannonce 
-                ORDER BY `date_creation` DESC";
+                ORDER BY `date_creation` ASC";
         $statement = self::$database->prepare($sql);
-        $statement->bindParam(":iduser",$id_user,PDO::PARAM_INT);
-        $statement->bindParam(":idannonce",$id_annonce,PDO::PARAM_INT);
+        $statement->bindParam(":iduser", $id_user, PDO::PARAM_INT);
+        $statement->bindParam(":idannonce", $id_annonce, PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetchAll();
     }
+
     #TODO refaire la fonction pour montrer les messages
 
-    public function CreateMessagefromEmeteur($id_user, $id_annonce, $reponse, $message, $id_user_annonce){
+    public function CreateMessagefromEmeteur($id_user, $id_annonce, $reponse, $message, $id_user_annonce)
+    {
         $sql = "INSERT INTO `message` (`id_message`, `detail`, `Annonce_id_Annonce`, `USER_id_user`, `reponse`, `Annonce_USER_id_user`) 
                 VALUES (NULL, :detail, :id_annonce, :id_user, :reponse, :id_user_annonce)";
         $statement = self::$database->prepare($sql);
@@ -179,5 +197,31 @@ class Database
     }
     #TODO refaire la fonction pour creer les messages
 
-}
+// Like
+    function like($id_user, $id_annonce)
+    {
+        $sql = "INSERT INTO `like` (`like`, `Annonce_id_Annonce`, `USER_id_user`) 
+            VALUES ('1', :id_annonce, :id_user)";
+        $statement = self::$database->prepare($sql);
+        $statement->execute(array(":id_annonce" => $id_annonce, ":id_user" => $id_user));
+    }
 
+    function unlike($id_user, $id_annonce)
+    {
+        $sql = "DELETE FROM `like` 
+            WHERE `like`.`Annonce_id_Annonce` = :id_annonce 
+            AND `like`.`USER_id_user` = :id_user ";
+
+        $statement = self::$database->prepare($sql);
+        $statement->execute(array(":id_annonce" => $id_annonce, ":id_user" => $id_user));
+    }
+
+    function affiche_like($id_user){
+        $sql = "SELECT * FROM `like` WHERE `USER_id_user` ={$id_user};";
+        $statement = self::$database->prepare($sql);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+
+
+}
