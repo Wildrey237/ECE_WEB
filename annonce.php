987e229cf115cdc99aba71d32068b09770b0f183
rayanejr <105799268+rayanejr@users.noreply.github.com>
2022-10-23 09:28:26 +0200

    tri+annonce+index fini

---
+++ annonce.php
@@ -0,0 +1,83 @@
+<?php
+
+  require_once('./controller/database.php');
+
+  $database = new Database();
+ 
+
+  $data = $database->getAnnonceByID($_GET['id']);
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Le bon coin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    
+        <?php
+            include ("modele/session.php");
+            require_once("views/navbar.php");
+        ?>
+  
+
+
+      <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder"><?php echo $data['nom_annonce'] ?></h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Voici les informations de l annonce qui vous intéresse.</p>
+                </div>
+            </div>
+        </header>
+
+        <div class="card mb-5">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                  <h1><?php echo $data['nom_annonce'] ?></h1>
+                                  <h3><?php echo $data['prix'] ?> €</h3>    
+                                </div>
+
+                                <div>
+                                <p class="text-muted"><?php echo $data['date_ajout'] ?></p>
+                                </div>
+                                    
+                                
+                            
+                            </div>
+                            <img src="'.$data['photo_article'].'" class="w-100" alt="">
+                        </div>
+                    </div>
+
+
+                    <div class="card mb-3">
+                        <div class="card-body"> 
+                         <p class="text-muted"><?php echo $data['detail'] ?></p>
+                         
+                        </div>
+                    </div>
+
+
+                    <div class="card mb-5">
+                        <div class="card-body  classe mx-auto">
+                        <a class="btn btn-success" href="" style="width: 250px">Acheter</a>
+                       
+
+                        </div>
+
+                    </div>
+
+            <?php
+            
+            require_once("views/footer.php");
+        ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -33,6 +33,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Le bon coin</title>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
@@ -48,7 +49,7 @@
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">Leboncoin</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">Weclome to our app</p>
+                    <p class="lead fw-normal text-white-50 mb-0">Bienvenue sur WERJ</p>
                 </div>
             </div>
         </header>
@@ -99,7 +100,7 @@
                               <input type="number" value="<?php echo $prix ?>" class="form-control"  id="price" name="prix">
                             </div>
                             <div class="form-group mb-3">
-                               <button class="btn btn-primary" type="submit">Rechercher</button>
+                               <button class="btn btn-primary" type="submit" name="rechercher">Rechercher</button>
                             </div>
 
                             </form>
@@ -116,7 +117,7 @@
 
                       <?php 
                       
-                      
+                      if(isset($_GET["rechercher"])){
                       $products = $database->getAnnonceByFilters( $keywords,$category,$prix );
 
                       foreach ($products as $key => $product) {
@@ -132,24 +133,75 @@
                                         <!-- Product name-->
                                         <h5 class="fw-bolder">'.$product['nom_annonce'].'</h5>
                                         <!-- Product price-->
-                                       '.$product['prix'].' $
+                                       '.$product['prix'].' €
                                     </div>
                                 </div>
                                 
                                 
                                 <!-- Product actions-->';
-                                if ( isset($_SESSION['id_user']) ) { 
-                                  echo '
-                                  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="annonce.php?id='.$product['id_Annonce'].'">plus d informations</a></div>
-                                  </div>
-                                  ';
-                                }
+                                if (isset($_SESSION["session"])) {
+                                
+                                  // if ( isset($_SESSION['id_user']) ) { 
+                                    echo '
+                                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                      <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="annonce.php?id='.$product['id_Annonce'].'">plus d informations</a></div>
+                                    </div>
+                                    ';
+                                }else{
+                                    echo'
+                                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                      <div class="text-center"><a class="btn btn-outline-danger mt-auto" >Vous n avait pas accès a l annonce. Connecter-vous</a></div>
+                                    </div>
+                                    ';
+                                  }
                                 
                             echo '</div>
                         </div>
                         ';
                       }
+                    }else{
+                      $products = $database->getAnnonce();
+
+                      foreach ($products as $key => $product) {
+
+                        echo '
+                        <div class="col-md-4 mb-5">
+                            <div class="card h-100">
+                                <!-- Product image-->
+                                <img class="card-img-top" src="'.$product['Media'].'" alt="...">
+                                <!-- Product details-->
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        <!-- Product name-->
+                                        <h5 class="fw-bolder">'.$product['nom_annonce'].'</h5>
+                                        <!-- Product price-->
+                                       '.$product['prix'].' €
+                                    </div>
+                                </div>
+                                
+                                
+                                <!-- Product actions-->';
+                                if (isset($_SESSION["session"])) {
+                               
+                                  // if ( isset($_SESSION['id_user']) ) { 
+                                    echo '
+                                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                      <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="annonce.php?id='.$product['id_Annonce'].'">plus d informations</a></div>
+                                    </div>
+                                    ';
+                                }else{
+                                    echo'
+                                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                      <div class="text-center"><a class="btn btn-outline-danger mt-auto">Vous n avait pas accès a l annonce. Connecter-vous</a></div>
+                                    </div>
+                                    ';
+                                  }
+                                
+                            echo '</div>
+                        </div>
+                        ';
+                      }
+                    }
 
                       
                       ?>
